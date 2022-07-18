6fd6b61626c3e8c6446730aa8644745d6aa0bc12
arepollo <kole.warn@gmail.com>
2022-07-18 13:42:13 -0600

    creds

--- dnac_config.py
+++ dnac_config.py
@@ -2,4 +2,4 @@ import os
 DNAC=os.environ.get('DNAC','172.23.72.130')
 DNAC_PORT=os.environ.get('DNAC_PORT',443)
 DNAC_USER=os.environ.get('DNAC_USER','admin')
-DNAC_PASSWORD=os.environ.get('DNAC_PASSWORD','Cisco123')
+DNAC_PASSWORD=os.environ.get('DNAC_PASSWORD','!W@nt@cc355n')
